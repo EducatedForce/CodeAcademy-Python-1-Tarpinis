d11084291f6190fe8081cca984044da46506f73f
EducatedForce <110552173+EducatedForce@users.noreply.github.com>
2022-12-06 21:52:12 +0200

    First task completed

--- Task 1/main.py
+++ Task 1/main.py
@@ -17,3 +17,13 @@ users = [
     {"id": '8', "name": 'Simon Peterson', "age": 30, "hasDog": False},
     {"id": '9', "name": 'Daniel Cane', "age": 51, "hasDog": True},
 ]
+
+
+def filter_dog_owners(users_list):
+    """Returns filtered list of users with pets."""
+    return [user for user in users_list if user["hasDog"]]
+
+
+def filter_adults(users_list):
+    """Returns filtered list of users with age of 18 or more."""
+    return [user for user in users_list if user["age"] >= 18]
