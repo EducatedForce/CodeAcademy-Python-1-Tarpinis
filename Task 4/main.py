c07b5c863ea5aaee4939d256cb1099307899e8b7
EducatedForce <110552173+EducatedForce@users.noreply.github.com>
2022-12-06 21:56:17 +0200

    Fourth task completed

--- Task 4/main.py
+++ Task 4/main.py
@@ -8,3 +8,29 @@
 
 # Metodas: 
 # wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins True, kitu atveju False. 
+
+
+class Movie:
+    """class Movie
+    Required arguments are title, director and budget.
+    Title and director arguments must be string type.
+    Budget argument must be integer type.
+    """
+
+    def __init__(self, title, director, budget):
+        self.title = title
+        self.director = director
+        self.budget = budget
+
+    def was_expensive(self):
+        """Returns true if budget was above 100M USD, false otherwise."""
+        if self.budget <= 10 ** 8:
+            return False
+        return True
+
+
+movie1 = Movie("The Shawshank Redemption", "Frank Darabont", 25_000_000)
+
+movie2 = Movie("Dune", "Denis Villeneuve", 165_000_000)
+
+movie3 = Movie("Avatar", "James Cameron", 237_000_000)
